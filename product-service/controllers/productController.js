b0a1a63d438f5cd3895369451916a110b45b23ea
it21296796 <99311998+it21296796@users.noreply.github.com>
2025-04-04 03:38:09 +0530

    srfwefwe

--- product-service/controllers/productController.js
+++ product-service/controllers/productController.js
@@ -3,7 +3,7 @@ const Product = require('../models/Product');
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.json(products);//pio
+    res.json(products);
   } catch (err) {
     res.status(500).send('Server Error');
   }
