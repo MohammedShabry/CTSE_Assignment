71d55ce2e29366d3555165f50998740f701a6d62
it21296796 <99311998+it21296796@users.noreply.github.com>
2025-04-17 15:45:32 +0530

    nnnm

--- product-service/controllers/productController.js
+++ product-service/controllers/productController.js
@@ -3,7 +3,7 @@ const Product = require('../models/Product');
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.json(products);//kjhk
+    res.json(products);//kjhkkjkjkjk
   } catch (err) {
     res.status(500).send('Server Error');
   }
