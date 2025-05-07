94c323ca7378614c4a7b96dc9fc45ab7ec4dad61
IT21233494 <minduleewickramasinghe@gmail.com>
2025-05-07 22:35:20 +0530

    fix: update product controller

--- product-service/controllers/productController.js
+++ product-service/controllers/productController.js
@@ -1,14 +1,16 @@
 const Product = require('../models/Product');
 
+//get all products
 exports.getAllProducts = async (req, res) => {
   try {
     const products = await Product.find();
-    res.json(products);//kjhkkjkjkjk
+    res.json(products);
   } catch (err) {
     res.status(500).send('Server Error');
   }
 };
 
+//create products
 exports.createProduct = async (req, res) => {
   const { name, price, description } = req.body;
   try {
